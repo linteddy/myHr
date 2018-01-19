f83a189c55deb095d91fd62aa318c62cabb875ce
Teddy Ngwenya <teddyngwenya@gmail.com>
2018-01-19 22:32:03 +0200

    add angular project for front-end and springboot project for the back-end

---
+++ tanget-ui/src/app/app.module.ts
@@ -0,0 +1,40 @@
+﻿import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {routing} from './app.routing';
+
+import {AlertComponent} from './_directives';
+import {AuthGuard} from './_guards';
+import {AlertService, AuthenticationService, UserService} from './_services';
+import {HomeComponent} from './home';
+import {LoginComponent} from './login';
+
+// used to create fake backend
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
