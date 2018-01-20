518fc8b58a5e9791f811f0122fddc1248b058731
Teddy Ngwenya <teddyngwenya@gmail.com>
2018-01-20 15:30:47 +0200

    Add User class

--- tangent-server/src/main/java/za/co/tangentsolutions/praticalassignment/domain/User.java
+++ tangent-server/src/main/java/za/co/tangentsolutions/praticalassignment/domain/User.java
@@ -1,4 +1,10 @@
 package za.co.tangentsolutions.praticalassignment.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private String username;
+    private String password;
+    private String token;
 }
