34c9d4504d797da9b0a9efbf644c6148a8175d85
Linda Ngwenya <linda.ngwenya@absa.co.za>
2018-01-22 09:50:51 +0200

    rename tangent-server to server

--- tangent-server/src/main/java/za/co/tangentsolutions/praticalassignment/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package za.co.tangentsolutions.praticalassignment.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 1_800_000; // 30 Minutes
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-
-}
